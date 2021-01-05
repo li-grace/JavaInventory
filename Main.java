6d4dce0d4b9466d793723213e3723e2e0726ab56
li-grace <gra.li@mail.utoronto.ca>
2021-01-04 21:12:24 -0500

    updated erro checking and cleaned code

--- Main.java
+++ Main.java
@@ -6,8 +6,7 @@ class Main {
 
 		Scanner in = new Scanner(System.in);
 		List inventoryList = new List();
-		//ArrayList<Data> inventoryList = new ArrayList<Data>();
-		String blah, newname, newbrand;
+		String buffer, newname, newbrand;
 		int newnum;
 
 		System.out.println("Welcome to the inventory tracker. Select:");
@@ -19,56 +18,39 @@ class Main {
 		int userOption = in.nextInt();
 		while (userOption != 5) {
 			switch (userOption) {
-			case (1):
-				System.out.print("Enter the item name: ");
-				blah = in.nextLine();
-				newname = in.nextLine();
+				case (1):
+					System.out.print("Enter the item name: ");
+					buffer = in.nextLine();
+					newname = in.nextLine();
 
-				System.out.printf("Enter the item brand: ");
-				//blah = in.nextLine();
-				newbrand = in.nextLine();
+					System.out.printf("Enter the item brand: ");
+					newbrand = in.nextLine();
 
-				System.out.printf("Enter the item quantity: ");
-				newnum = in.nextInt();
+					System.out.printf("Enter the item quantity: ");
+					newnum = in.nextInt();
 
-				Data newEntry = new Data(newname, newbrand, newnum);
-				inventoryList.append(newEntry);
-				break;
+					Data newEntry = new Data(newname, newbrand, newnum);
+					inventoryList.append(newEntry);
+					break;
 
-			case (2):
-				//int total = inventoryList.size();
-				/*System.out.printf("Displaying all %d items:\n",inventoryList.getSize());
-				for (int i = 0; i < total; i++) {
-					System.out.printf("%d. ",i+1);
-					inventoryList.get(i).print();
-				}*/
-				inventoryList.print();
-				break;
-			case (3):
-				System.out.print("Enter the number of the entry you wish to delete: ");
-				newnum = in.nextInt();
-				newnum--;
-				System.out.printf("Are you sure you want to delete ");
-				inventoryList.getData(newnum).print();
-				System.out.printf("? y or n: \n");
-				blah = in.nextLine();
-				newname = in.nextLine();
-				if (newname.equals("y")){
-					/*if (newnum < inventoryList.size() && newnum >= 0) {
-						inventoryList.remove(newnum);
-						System.out.println("It has been removed.");
-					}
-					else {
-						System.out.println("Sorry! This item doesn't exist.");
-					}*/
+				case (2):
+					inventoryList.print();
+					break;
+
+				case (3):
+					System.out.print("Enter the number of the entry you wish to delete: ");
+					newnum = in.nextInt();
+					newnum--;
 					inventoryList.delete(newnum);
-				}
-				break;
-			case (4):
-				System.out.println("Thanks for using the inventory tracker! :)");
-				userOption = 5;
-				return;
+
+					break;
+
+				case (4):
+					System.out.println("Thanks for using the inventory tracker! :)");
+					userOption = 5;
+					return;
 			}
+
 			System.out.println();
 			System.out.println("1. Add new entry");
 			System.out.println("2. View all entries");
@@ -77,11 +59,5 @@ class Main {
 
 			userOption = in.nextInt();
 		}
-
-
-
-
-
-
   }
 }
